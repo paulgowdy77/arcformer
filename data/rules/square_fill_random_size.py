53d87991fd14bbaee2aa24bd51c4a3df8cce7adb
Paul Gamble <paulgamble100@gmail.com>
2024-02-27 21:08:20 -0800

    more square rules

---
+++ data/rules/square_fill_random_size.py
@@ -0,0 +1,33 @@
+import numpy as np
+import random
+
+def generate_config():
+    config = {
+    
+    }
+    return config
+
+
+
+def generate_example(config):
+
+    colors = random.sample(range(0,9), 2)
+
+    input = np.ones((16, 16)) * colors[0]
+    output = np.ones((16, 16)) * colors[1]
+
+
+
+    # random square size
+    square_size = random.randint(1, 5)
+
+    # generate a square in a random position in the input
+    x = random.randint(1, 16 - square_size)
+    y = random.randint(1, 16 - square_size)
+    input[y:y+square_size, x:x+square_size] = colors[1]
+    
+
+    return input, output
+
+def example_func():
+    return generate_example
--- data/visualize_single_rule.py
+++ data/visualize_single_rule.py
@@ -4,7 +4,7 @@ import os
 from rule import Rule
 from plotter_utils import display_example_list
 
-rule_filename = 'rules.square_remove'
+rule_filename = 'rules.square_to_output'
 
 rule_module = importlib.import_module(rule_filename)
 config = rule_module.generate_config()
