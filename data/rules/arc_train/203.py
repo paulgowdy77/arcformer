7244b751de9bb662697ecd3da04fa7fa4e172736
Paul Gamble <paulgamble100@gmail.com>
2024-03-17 15:11:55 -0700

    fixed 203

--- data/rules/arc_train/203.py
+++ data/rules/arc_train/203.py
@@ -16,7 +16,19 @@ def generate_config():
 
 def generate_example(config):
 
+    nb_colors = random.randint(2, 7)
+    colors = random.sample(range(0, 9), nb_colors)
+    dim = nb_colors * 2
 
+    input = np.ones((dim, dim), dtype=int) * colors[0]
+    output = np.ones((dim, dim), dtype=int) * colors[0]
+
+    for i in range(nb_colors):
+        margin = dim - i
+        input[i:margin, i:margin] = colors[i]
+
+        # output is the opposite
+        output[i:margin, i:margin] = colors[nb_colors - i - 1]
 
 
     return input, output
