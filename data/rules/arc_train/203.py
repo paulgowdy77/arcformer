771a4b88c10e3a6ec7456c2d660d363768dde504
Paul Gamble <paulgamble100@gmail.com>
2024-03-17 14:46:19 -0700

    start 203

---
+++ data/rules/arc_train/203.py
@@ -0,0 +1,27 @@
+import numpy as np
+import random
+
+# ARC Public training set 117
+
+def generate_config():
+    #colors = random.sample(range(0, 9), 2)
+
+    config = {
+    }
+
+    return config
+
+
+
+
+def generate_example(config):
+
+
+
+
+    return input, output
+
+
+
+def example_func():
+    return generate_example
