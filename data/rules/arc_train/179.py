3d2905b021271ce8bc618ca58d531c51db4c3a14
Paul <paulgamble100@gmail.com>
2024-03-27 07:44:03 -0700

    train 179

--- data/rules/arc_train/179.py
+++ data/rules/arc_train/179.py
@@ -15,49 +15,23 @@ def generate_config():
 
 def generate_example(config):
 
-    colors = random.sample(range(0, 9), 7)
+    colors = random.sample(range(0, 9), 4)
 
-    input = np.ones((9, 9), dtype=int) * colors[0]
-    output = np.ones((6, 6), dtype=int) * colors[0]
+    dim = (2 * random.randint(3, 6)) - 1
 
-    input[:,2] = colors[1]
-    input[2,:] = colors[1]
+    input = np.ones((dim, dim), dtype=int) * colors[0]
+    
+    for i in range(dim):
+        for j in range(dim):
+            input[i, j] = random.choice(colors)
 
-    last_four_colors = colors[3:]
+    # mirror inptu along diagonal
+    output = np.zeros((dim, dim), dtype=int)
+    for i in range(dim):
+        for j in range(dim):
+            output[i, j] = input[j, i]
 
-    for i in range(6):
-        for j in range(6):
-            if random.random() < 0.65:
-                input[3+i, 3+j] = colors[2]
-                if i < 3 and j < 3:
-                    output[i, j] = last_four_colors[0]
-                elif i < 3 and j >= 3:
-                    output[i, j] = last_four_colors[1]
-                elif i >= 3 and j < 3:
-                    output[i, j] = last_four_colors[2]
-                else:
-                    output[i, j] = last_four_colors[3]
-
-    input[0,0] = colors[3]
-    input[0,1] = colors[4]
-    input[1,0] = colors[5]
-    input[1,1] = colors[6]
-
-    # randomly flip the input and output
-    if random.random() < 0.5:
-        input = np.flip(input, 0)
-        output = np.flip(output, 0)
-    if random.random() < 0.5:
-        input = np.fliplr(input)
-        output = np.fliplr(output)
-    if random.random() < 0.5:
-        input = np.flipud(input)
-        output = np.flipud(output)
-    if random.random() < 0.5:
-        x = random.randint(0, 3)
-        input = np.rot90(input, x)
-        output = np.rot90(output, x)
-                
+    
    
     
 
